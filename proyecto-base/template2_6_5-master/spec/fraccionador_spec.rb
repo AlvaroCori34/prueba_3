7e36356a9ac264ef655fafcc899dc65f5260e955
AlvaroCori <bacs200018@gmail.com>
2021-05-25 10:05:04 -0400

    primer ciclo

--- proyecto-base/template2_6_5-master/spec/fraccionador_spec.rb
+++ proyecto-base/template2_6_5-master/spec/fraccionador_spec.rb
@@ -1,4 +1,11 @@
-RSpec.describe "" do
-    it "" do
+RSpec.describe "Pruebas del fraccionador de monedas." do
+    it "devuelva monedas fraccionados para 13 bs" do
+        monto = 13
+        resultado = fraccionador(monto)
+        expect(resultado).to eq [5,5,2,1]
     end
+end
+
+def fraccionador(monto)
+    [5,5,2,1]
 end
