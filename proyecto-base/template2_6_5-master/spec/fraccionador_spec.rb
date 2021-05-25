53b2e64565d128421a445302b11ef3359f4ab5da
AlvaroCori <bacs200018@gmail.com>
2021-05-25 10:17:56 -0400

    cuarto ciclo

--- proyecto-base/template2_6_5-master/spec/fraccionador_spec.rb
+++ proyecto-base/template2_6_5-master/spec/fraccionador_spec.rb
@@ -14,31 +14,28 @@ RSpec.describe "Pruebas del fraccionador de monedas." do
         resultado = fraccionador(monto)
         expect(resultado).to eq [5,5,5,5,2]
     end
+    it "devuelva monedas fraccionadas para 43 bs" do
+        monto = 43
+        resultado = fraccionador(monto)
+        expect(resultado).to eq [5,5,5,5,5,5,5,5,2,1]
+    end
 end
 
 def fraccionador(monto)
     monedas = []
     while (monto>0) do 
-        if (monto>=5)
+        case 
+        when monto>=5
             monedas.push(5)
             monto = monto - 5
-        else
-            if (monto>=2)
-                monedas.push(2)
-                monto = monto - 2
-            else
-                if (monto==1)
-                    monedas.push(1)
-                    monto = monto - 1
-                end
-            end
+
+        when monto>=2 
+            monedas.push(2)
+            monto = monto - 2
+        when monto== 1
+            monedas.push(1)
+            monto = monto - 1
         end
-    end
-    if (monto==13)
-        return [5,5,2,1]
-    end
-    if (monto==19) 
-        return [5,5,5,2,2]
     end
     return monedas
 end
